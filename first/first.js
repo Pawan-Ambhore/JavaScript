355d4a88e657ab0c605030b70de62ee8e6167ab7
Pawan Ambhore <pawan.ambhore19@vit.edu>
2022-11-20 14:41:13 +0530

    choose random color

---
+++ first/first.js
@@ -0,0 +1,10 @@
+console.log('Pawan');
+
+
+
+const colors = ["red", "blue", "black", "pink", "yellow"];
+const randomIndex = Math.floor(Math.random() * colors.length);
+const randomColor = colors[randomIndex];
+
+console.log('The color is ' + randomColor);
+console.log(randomIndex);   
